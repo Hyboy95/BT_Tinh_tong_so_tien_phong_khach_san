0700ebcbee184a2bf412410ac460da024f3c94ca
Hyboy95 <temilator@gmail.com>
2023-04-18 10:34:24 +0700

    first commit

---
+++ matrixElementSum.ts
@@ -0,0 +1,16 @@
+function matrixElementSum(matrix: number[][]) {
+    let sum: number = 0;
+    for (let i = 0; i < matrix.length - 1; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i+1][j] = 0;
+            }
+            sum += matrix[i][j];
+        }
+    }
+
+    return sum;
+}
+
+let matrix = [[0,1,1,2],[0,5,0,0],[2,0,3,3],[3,4,5,6]];
+console.log(matrixElementSum(matrix))
