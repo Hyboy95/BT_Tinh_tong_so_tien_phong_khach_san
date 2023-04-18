fed2aa3fc8a718850cd307628f5d570aef37108d
Hyboy95 <temilator@gmail.com>
2023-04-18 11:04:25 +0700

    second commit

--- matrixElementSum.ts
+++ matrixElementSum.ts
@@ -1,8 +1,8 @@
 function matrixElementSum(matrix: number[][]) {
     let sum: number = 0;
-    for (let i = 0; i < matrix.length - 1; i++) {
+    for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] === 0) {
+            if (matrix[i][j] === 0 && i < matrix.length - 1) {
                 matrix[i+1][j] = 0;
             }
             sum += matrix[i][j];
@@ -12,5 +12,9 @@ function matrixElementSum(matrix: number[][]) {
     return sum;
 }
 
-let matrix = [[0,1,1,2],[0,5,0,0],[2,0,3,3],[3,4,5,6]];
+let matrix = [
+    [0,1,1,2],
+    [0,5,0,0],
+    [2,0,3,3]
+];
 console.log(matrixElementSum(matrix))
